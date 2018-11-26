eff3062ca9c892916b54ab22d9273645517e0f9e
Joubin Jabbari <joubin.j@gmail.com>
2018-11-25 20:51:54 -0800

    Demo Video how to use Git

--- scripts/build_run_webgoat.sh
+++ scripts/build_run_webgoat.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
-
 pushd ..
-
 mvn clean install -DskipTests=true
-
 cd webgoat-server
-
 docker build -t webgoat/webgoat-v8.0.0.snapshot .
-
 popd
-
 pushd ..
-
 docker-compose rm -f
 docker-compose -f docker-compose-local-webgoat.yml up
-
 popd
